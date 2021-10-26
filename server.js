ce90b85157acd651bcb509654a0d2310531e264f
egwe956 <65057481+egwe956@users.noreply.github.com>
2021-10-26 23:03:43 +0800

    async posting works

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 // const Article = require("./models/article");
 const articleRouter = require("./routes/articles");
 // const methodOverride = require("method-override");
 const app = express();
 
-// mongoose.connect("mongodb://localhost/blog", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-// });
+mongoose.connect("mongodb://localhost/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // useCreateIndex: true,
+});
 
 app.set("view engine", "ejs");
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 // app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
       description: "Test Description",
     },
   ];
-  res.render("index", { articles: articles });
+  res.render("articles/index", { articles: articles });
 });
 
 app.use("/articles", articleRouter);
